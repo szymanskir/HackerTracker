b50b213fd2a37bb3b38277e9e7914710815bfa6e
szymanskir <ryszard.szymanski@outlook.com>
2019-12-31 15:06:36 +0100

    Deployment script fix due to renv and github packages issue

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
+rm .Rprofile
+Rscript -e 'renv::restore()'
+Rscript -e 'devtools::install_github("JohnCoene/sigmajs", force = TRUE)'
 Rscript -e 'rsconnect::setAccountInfo(name=Sys.getenv("SHINYAPP_NAME"), secret=Sys.getenv("SHINYAPP_SECRET"), token=Sys.getenv("SHINYAPP_TOKEN"))'
 Rscript -e 'rsconnect::deployApp(appName="hackertracker", appFiles = grep(list.files(path=".", recursive = TRUE, all.files = T), pattern="renv|rsconnect|.Rproj|.Rprofile|.git|manifest.json", inv=T, value=T))'
