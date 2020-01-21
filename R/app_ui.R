4178358328877a01499403cb591812ea70460638
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2020-01-21 23:05:39 +0100

    fixed title in web page name

--- R/app_ui.R
+++ R/app_ui.R
@@ -4,7 +4,7 @@ app_ui <- function() {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
+    # List the first level UI elements here
     dashboardPage(
       dashboardHeader(title = span(tagList(icon("desktop"), "Hacker Tracker"))),
       dashboardSidebar(disable = TRUE),
@@ -12,24 +12,23 @@ app_ui <- function() {
         fluidPage(
           mod_general_overview_page_ui("general_page")
         )
-      )
+      ),
+      title="Hacker Tracker"
     )
   )
 }
 
 #' @import shiny
 golem_add_external_resources <- function() {
-  addResourcePath(
-    'www', system.file('app/www', package = 'HackerTracker')
-  )
- 
+  addResourcePath('www', system.file('app/www', package = 'HackerTracker'))
+  
   tags$head(
     golem::activate_js(),
     golem::favicon(),
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
-    tags$link(rel="stylesheet", type="text/css", href="www/styles.css")
+    tags$link(rel = "stylesheet", type = "text/css", href = "www/styles.css")
   )
   
 }
