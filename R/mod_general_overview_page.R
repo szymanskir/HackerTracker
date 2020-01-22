f45b701f5c663e365cf5797df393529c351dff4f
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2020-01-23 00:29:49 +0100

    added story types

--- R/mod_general_overview_page.R
+++ R/mod_general_overview_page.R
@@ -61,13 +61,12 @@ mod_general_overview_page_ui <- function(id) {
 #' 
 #' 
 #' @importFrom future future value
-#' @importFrom hackeRnews get_comments get_top_stories
+#' @importFrom hackeRnews get_comments
 #' 
     
 mod_general_overview_page_server <- function(input, output, session) {
   ns <- session$ns
   
-  top_stories_promise <- reactiveVal()
   comments_promise <- reactiveVal()
   data_fetching_interval_event <- reactiveTimer(DATA_FETCHING_INTERVAL)
   
@@ -83,23 +82,23 @@ mod_general_overview_page_server <- function(input, output, session) {
   
   observe({
     data_fetching_interval_event()
-    top_stories_promise(future(get_top_stories(max_items = 10)))
   })
   
-  observe({
-    req(stories_table$selected_story())
-    req(top_stories_promise())
-    
-    top_stories <- value(top_stories_promise())
-    selected_top_story <- top_stories[[stories_table$selected_story()]]
-    comments_with_sentiment_promise <- future(get_comments(selected_top_story)) %...>%
-      mutate(sentiment = calculate_sentiment(text) %>% round(digits = 2) %>% min(10) %>% max(-10))
-      
+  clip <- function(x, min_value, max_value) {
+    ifelse(x <= min_value,  min_value, ifelse(x >= max_value, max_value, x))
+  }
+
+  observeEvent(stories_table$selected_story(), {
+    selected_story <- stories_table$selected_story()
+    req(selected_story)
+    comments_with_sentiment_promise <- future(get_comments(selected_story)) %...>%
+      mutate(sentiment = clip(calculate_sentiment(text), -10, 10) %>% round(digits = 2))
     comments_promise(comments_with_sentiment_promise)
-  })
+  }
+  , ignoreInit = TRUE)
   
   # Modules
-  stories_table <- callModule(mod_stories_table_server, "stories_table", stories_promise = top_stories_promise)
+  stories_table <- callModule(mod_stories_table_server, "stories_table")
   comments_table <- callModule(mod_comments_table_server, "comments_table", comments_promise = comments_promise)
   callModule(mod_sentiment_distribution_plot_server, "sentiment_plotter", comments_promise = comments_promise)
   comments_graph <- callModule(
--- R/mod_stories_table.R
+++ R/mod_stories_table.R
@@ -12,7 +12,7 @@
 #'
 #' @keywords internal
 #' @export 
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList selectInput actionLink
 #' @importFrom shinydashboard box
 #' @importFrom shinycssloaders withSpinner
 mod_stories_table_ui <- function(id) {
@@ -20,6 +20,12 @@ mod_stories_table_ui <- function(id) {
   tagList(
     box(
       actionLink(ns("help"), "Start here!"),
+      selectInput(
+        ns("story_type"),
+        "Select stories type:",
+        choices = c('Top stories' = 'top', 'Best stories' = 'best', 'New Stories' = 'new'),
+        selected = 'top'
+      ),
       rintrojs::introBox(
         withSpinner(
           DT::dataTableOutput(ns("stories_table"))
@@ -44,13 +50,40 @@ mod_stories_table_ui <- function(id) {
 #' @keywords internal
 #' 
 #' @import promises
+#' @importFrom hackeRnews get_comments get_top_stories get_new_stories get_best_stories
+
     
-mod_stories_table_server <- function(input, output, session, stories_promise) {
+mod_stories_table_server <- function(input, output, session) {
   ns <- session$ns
   
+  dataSource <- reactiveVal()
+  selected_story_r <- reactiveVal()
+  
+  observeEvent(input$story_type, {
+    req(input$story_type)
+    selected_story_r(NULL)
+    max_items <- 10
+    if (input$story_type == "top") {
+      dataSource(future(get_top_stories(max_items = max_items)))
+    }
+    else if (input$story_type == "best") {
+      dataSource(future(get_best_stories(max_items = max_items)))
+    }
+    else if (input$story_type == "new") {
+      dataSource(future(get_new_stories(max_items = max_items)))
+    }
+  })
+  
+  observeEvent(input$stories_table_row_last_clicked, {
+    stories <- value(dataSource())
+    selected_story_r(stories[[input$stories_table_row_last_clicked]])
+  }, ignoreInit = TRUE, ignoreNULL = TRUE)
+  
+  
+  
   output$stories_table <- DT::renderDataTable({
-    req(stories_promise())
-    stories_promise() %...>%
+    req(dataSource())
+    dataSource() %...>%
       lapply(function(item) {
         data.frame(
           title = item$title,
@@ -59,7 +92,7 @@ mod_stories_table_server <- function(input, output, session, stories_promise) {
         )
       }) %...>%
       do.call(rbind, .) %...>%
-      DT::datatable(selection = "single")
+      DT::datatable(selection = "single", options = list(lengthChange = FALSE, pageLength = 5))
   })
   
   observeEvent(input$help, {
@@ -67,6 +100,6 @@ mod_stories_table_server <- function(input, output, session, stories_promise) {
   })
   
   list(
-    selected_story = reactive({input$stories_table_row_last_clicked})
+    selected_story = reactive({ selected_story_r()})
   )
 }
